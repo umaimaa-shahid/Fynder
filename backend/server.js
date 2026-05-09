8fd4bf7e5bcf3420e793c0bb0017d05292a34b13
Umaima Shahid <umaima.shahid2023@gmail.com>
2026-05-09 19:09:54 +0500

    feat(server): update server configuration and middleware setup

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ import cors from "cors";
 
 import connectDB from "./config/db.js";
 
+import { createServer } from "http";
+import { initSocket } from "./socket/index.js";
+
+import userRoutes    from "./routes/userRoutes.js";
+import groupRoutes   from "./routes/groupRoutes.js";
+import chatRoutes    from "./routes/chatRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
@@ -17,10 +22,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const httpServer = createServer(app);
+initSocket(httpServer);
+
+
+
 // ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
-
+app.use("/api/user",    userRoutes);
+app.use("/api/group",   groupRoutes);
+app.use("/api/chat",    chatRoutes);
 // TEST ROUTE
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -28,8 +40,8 @@ app.get("/", (req, res) => {
 
 // PORT
 const PORT = process.env.PORT || 5000;
-console.log("RESEND KEY:", process.env.RESEND_API_KEY);
 // SERVER
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("RESEND KEY:", process.env.RESEND_API_KEY);
 });
