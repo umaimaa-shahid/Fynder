83e0d09b051cef9a08d7e43e00b41a81e580f706
Umaima Shahid <umaima.shahid2023@gmail.com>
2026-05-09 19:07:12 +0500

    feat (models): update User, Group, and Message schemas

--- backend/models/User.js
+++ backend/models/User.js
@@ -48,4 +48,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+export default mongoose.models.User || mongoose.model("User", userSchema);
